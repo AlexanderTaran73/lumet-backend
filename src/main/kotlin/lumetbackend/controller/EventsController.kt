4a39cef7db06c98f688bf2b8ab4f128b845af872
AlexanderTaran73 <alexhock@yandex.ru>
2023-02-02 13:23:02 +0300

    Post -> Git (typo corrected)

--- src/main/kotlin/lumetbackend/controller/EventsController.kt
+++ src/main/kotlin/lumetbackend/controller/EventsController.kt
@@ -30,27 +30,27 @@ class EventsController(private val eventChangesService: EventChangesService) {
         return eventChangesService.changeEvent(createEventRequest, request, eventId)
     }
 
-    @PostMapping("/get_All_events")
+    @GetMapping("/get_All_events")
     fun getAllEvents(request: HttpServletRequest): ResponseEntity<Any>{
         return eventChangesService.getAllEvents(request)
     }
 
-    @PostMapping("/get_All_events/sort_and_search/{search}/{min_age}/{rating}/{hobby}/{users_limit}")
+    @GetMapping("/get_All_events/sort_and_search/{search}/{min_age}/{rating}/{hobby}/{users_limit}")
     fun getAllEventsSort(request: HttpServletRequest, @PathVariable @NotNull search:String, @PathVariable @NotNull min_age:Int, @PathVariable @NotNull rating:Int, @PathVariable @NotNull hobby:String, @PathVariable @NotNull users_limit:Int): ResponseEntity<Any>{
         return eventChangesService.getAllEventsSort(request, search, min_age, rating, hobby,users_limit)
     }
 
-    @PostMapping("/get_events_list_by_id")
+    @GetMapping("/get_events_list_by_id")
     fun getEventsById(request: HttpServletRequest, eventIdList: List<Int>): ResponseEntity<Any> {
         return eventChangesService.getEventsById(request, eventIdList)
     }
 
-    @PostMapping("/get_event_by_id/{eventId}")
+    @GetMapping("/get_event_by_id/{eventId}")
     fun getEventById(request: HttpServletRequest, @PathVariable @NotNull eventId: Int): ResponseEntity<Any> {
         return eventChangesService.getEventById(request, eventId)
     }
 
-    @PostMapping("/get_created_events")
+    @GetMapping("/get_created_events")
     fun getCreaterEvents(request: HttpServletRequest): ResponseEntity<Any>{
         return eventChangesService.getCreaterEvents(request)
     }
